145fe2396af084d65ce7ddadca1250fd2cdbcfc7
Kuldeep Gupta <kuldeep.gupta@thewasserstoff.com>
2024-08-31 14:00:39 +0530

    user

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,83 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AuctionList from "./components/AuctionList";
+import AuctionItem from "./components/AuctionItem";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import BidForm from "./components/BidForm";
+import Logout from "./components/Logout";
+import ProtectedRoute from "./components/ProtectedRoute";
+import { useState, useEffect } from "react";
+import { AuthProvider } from "./contexts/index";
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import CreateAuctionItem from "./components/CreateAuctionItem";
+import EditAuctionItem from "./components/EditAuctionItem";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	const login = () => {
+		setIsLoggedIn(true);
+	};
+
+	const logout = () => {
+		setIsLoggedIn(false);
+	};
+
+	const token = document.cookie
+		.split("; ")
+		.find((row) => row.startsWith("jwt="))
+		?.split("=")[1];
+
+	useEffect(() => {
+		if (token) {
+			login();
+		} else {
+			logout();
+		}
+	}, [token]);
+
+	return (
+		<AuthProvider value={{ isLoggedIn, login, logout }}>
+			<Router>
+				<NavBar />
+				<div className="container bg-[#1b1b25] pt-16"> {/* Add padding-top */}
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/signup" element={<Signup />} />
+						<Route path="/login" element={<Login />} />
+						<Route
+							path="/profile"
+							element={<ProtectedRoute component={Profile} />}
+						/>
+						<Route path="/logout" element={<Logout />} />
+						<Route path="/auctions" element={<AuctionList />} />
+						<Route
+							path="/auction/:id"
+							element={<ProtectedRoute component={AuctionItem} />}
+						/>
+						<Route
+							path="/auction/create"
+							element={
+								<ProtectedRoute component={CreateAuctionItem} />
+							}
+						/>
+						<Route
+							path="/auction/edit/:id"
+							element={
+								<ProtectedRoute component={EditAuctionItem} />
+							}
+						/>
+						<Route
+							path="/auction/bid/:id"
+							element={<ProtectedRoute component={BidForm} />}
+						/>
+					</Routes>
+				</div>
+			</Router>
+		</AuthProvider>
+	);
 }
 
-export default App
+export default App;
